4d02c343e03c38781ff0e9aae09743c16fcb5288
Tahir Shaikh <shaikhtahir0418@gmail.com>
2025-08-27 17:09:15 +0530

    third commit

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import {protectedRoute} from "../middleware/audit,js";
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController";
+import {protectRoute} from "../middleware/auth.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
 
 
 const messageRouter = express.Router();
 
-messageRouter.get("/users", protectedRoute, getUsersForSidebar);
-messageRouter.get("/:id", protectedRoute, getMessages);
-messageRouter.put("mark/:id", protectedRoute, markMessageAsSeen);
+messageRouter.get("/users", protectRoute, getUsersForSidebar);
+messageRouter.get("/:id", protectRoute, getMessages);
+messageRouter.put("mark/:id", protectRoute, markMessageAsSeen);
+messageRouter.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRouter;
 
